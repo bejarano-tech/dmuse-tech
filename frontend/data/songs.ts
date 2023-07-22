5ec54753857e40e77192f3252d5e50c871e71eb4
Frederick Bejarano Sanchez <bejarano.frederick@gmail.com>
2023-07-22 10:34:37 -0500

    add songs

--- frontend/data/songs.ts
+++ frontend/data/songs.ts
@@ -28,5 +28,40 @@ export const songs: SongItem[] = [
     image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/buenas-noticias.png",
     name: "Buenas noticias",
     attributes: [{trait_type: "artist", value: "L.L.D"}]
-  }
+  },
+  {
+    description: "El Encuentro - Song by L.L.D",
+    external_url: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/el-encuentro.wav",
+    image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/el-encuentro.png",
+    name: "El Encuentro",
+    attributes: [{trait_type: "artist", value: "L.L.D"}]
+  },
+  {
+    description: "La Noche - Song by L.L.D",
+    external_url: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/la-noche.wav",
+    image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/la-noche.png",
+    name: "La Noche",
+    attributes: [{trait_type: "artist", value: "L.L.D"}]
+  },
+  {
+    description: "Los Colores - Song by L.L.D",
+    external_url: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/los-colores.wav",
+    image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/los-colores.png",
+    name: "Los Colores",
+    attributes: [{trait_type: "artist", value: "L.L.D"}]
+  },
+  {
+    description: "Sigo Chill - Song by L.L.D",
+    external_url: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/sigo-chill.wav",
+    image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/sigo-chill.png",
+    name: "Sigo Chill",
+    attributes: [{trait_type: "artist", value: "L.L.D"}]
+  },
+  {
+    description: "Tu Pañuelo - Song by L.L.D",
+    external_url: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/tu-panuelo.wav",
+    image: "https://scarlet-brilliant-roundworm-590.mypinata.cloud/ipfs/QmePtaWQdj3KJMVDQv3w4sBMTpVhJX1V2tmZjnWVQirG7K/tu-panuelo.png",
+    name: "Tu Pañuelo",
+    attributes: [{trait_type: "artist", value: "L.L.D"}]
+  },
 ]
